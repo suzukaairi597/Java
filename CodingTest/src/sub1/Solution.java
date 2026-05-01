89ff035223695473198532a78d3d39b74f6917a9
suzukaairi597 <suzukaairi597@gmail.com>
2026-05-01 15:04:05 +0900

    CodingTest

---
+++ CodingTest/src/sub1/Solution.java
@@ -0,0 +1,20 @@
+package sub1;
+
+/*
+ * 날짜 : 2026/05/01
+ * 이름 : 신여진 
+ * 내용 : PCCE 문자 출력
+ */
+
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		
+		String message = "Let's go!";
+		
+		System.out.println("3\n2\n1");
+		System.out.println(message);
+		
+	}
+}
