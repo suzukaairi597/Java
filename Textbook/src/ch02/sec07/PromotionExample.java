0aeaaf5c44fdd2458c9119623ca7847c040ce842
suzukaairi597 <suzukaairi597@gmail.com>
2026-05-01 17:21:09 +0900

    Textbook

--- Textbook/src/ch02/sec07/PromotionExample.java
+++ Textbook/src/ch02/sec07/PromotionExample.java
@@ -1,6 +1,7 @@
 package ch02.sec07;
 
 public class PromotionExample {
+	
 	public static void main(String[] args) {
 		
 		byte byteValue = 10;
@@ -18,17 +19,11 @@ public class PromotionExample {
 		longValue = 100;
 		float floatValue = longValue;
 		
-		System.out.println("floatValue: ");
+		System.out.println("floatValue: " + floatValue);
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		floatValue = 100.5F;
+		double doubleValue = floatValue;
+		System.out.println("doubleValue: " + doubleValue);
 		
 		
 		
