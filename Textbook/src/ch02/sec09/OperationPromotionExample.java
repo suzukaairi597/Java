1cdefe828f57ad4ff788a421ce4ff1023a00bddf
suzukaairi597 <suzukaairi597@gmail.com>
2026-05-01 17:48:32 +0900

    add Textbook

--- Textbook/src/ch02/sec09/OperationPromotionExample.java
+++ Textbook/src/ch02/sec09/OperationPromotionExample.java
@@ -1,5 +1,26 @@
 package ch02.sec09;
 
 public class OperationPromotionExample {
+	public static void main(String[] args) {
+		byte result1 = 10 + 20;
+		System.out.println("result1: " + result1);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
