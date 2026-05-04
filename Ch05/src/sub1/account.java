acea72b9ec06be934ea39732c86075ee8480849c
suzukaairi597 <suzukaairi597@gmail.com>
2026-05-04 11:17:52 +0900

    Ch05

--- Ch05/src/sub1/account.java
+++ Ch05/src/sub1/account.java
@@ -1,5 +1,27 @@
 package sub1;
 
 public class account {
-
+	//속성(필드)
+	String bank;
+	String id;
+	String name;
+	int balance;
+	
+	//기능
+	public void withdraw(int amount) {
+		this.balance -= amount;
+	
+	}
+	
+	public void deposit(int amount) {
+		this.balance += amount;
+		
+	}
+	
+	public void show() {
+		System.out.println("은행명 : " + this.bank);
+		System.out.println("계좌번호 : " + this.id);
+		System.out.println("입금주 : " + this.name);
+		System.out.println("현쟈잔액 : " + this.balance);
+	}
 }
