7e361a8af67da479e92ea170e0a30c78d3f015eb
suzukaairi597 <suzukaairi597@gmail.com>
2026-05-08 10:43:38 +0900

    TEST

---
+++ TEST/src/Test1/Test08.java
@@ -0,0 +1,28 @@
+package Test1;
+
+/*
+ * 날짜 : 2026/05/08
+ * 이름 : 신여진
+ * 내용 : 자바 반복문 연습문제
+ */
+
+public class Test08 {
+	public static void main(String[] args) {
+		
+		int n = 5;
+		
+		for(int i = 0; i < n; i++) {
+			
+			for(int j=n-1; j>i; j--) {
+				System.out.print(" ");
+			}
+		
+			for(int j = 0; j < 2 * i + 1; j++) {
+				System.out.print("*");
+			}
+		
+			System.out.print("\n");
+			
+		}
+	}
+}
